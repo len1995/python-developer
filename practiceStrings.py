84ee6c300520c5fc795a44f1634ca813bed99046
Aileen Grace Rabasto <agdrabasto@gmail.com>
2023-09-22 22:28:24 +0800

    commit saved

--- practiceStrings.py
+++ practiceStrings.py
@@ -1,5 +1,9 @@
 first_name='AILEEN'
 last_name='RABASTO'
 
-output='Hello'+' '+first_name+' '+last_name
+#output='Hello'+' '+first_name+' '+last_name
+#output='Hello,{} {}'.format(first_name, last_name)
+#output='Hello, {0} {1}'.format(first_name, last_name)
+#for python 3
+output=f'Hello, {first_name} {last_name}'
 print(output)
