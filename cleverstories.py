8f41175c4442453a3ab5283e39d082695c11399d
Aileen Grace Rabasto <agdrabasto@gmail.com>
2023-09-23 01:17:19 +0800

    added inputs to program

---
+++ cleverstories.py
@@ -0,0 +1,23 @@
+#Author: Aileen Grace D. Rabasto
+#Project 01: Clever Stories
+#changes: Added another paragraph on what the family did when they saw the animal. Added inputs for 1 adjective, 2 verbs, 2 family members and 1 room in the house to make the story more interesting.
+print('MAD LIBS')
+print('Please enter the following:')
+adj=input('adjective:')
+ani=input('animal: ')
+verb=input('verb: ')
+excl=input('exclamation: ')
+verb1=input('verb: ')
+verb2=input('verb: ')
+adj1=input('adjective: ')
+fam1=input('family member: ')
+verb3=input('verb:')
+fam2=input('family member: ')
+verb4=input('verb:')
+time=input('length of time:')
+room=input('Room in the house: ')
+print()
+print('Your story is:')
+print()
+print(f'The other day, I was really in trouble. It all started when I saw a very {adj} {ani} {verb} down the hallway."{excl}!" I yelled. But all I could think to do was to {verb1} over and over. Miraculously, that caused it to stop, but not before it tried to {verb2} right in front of my family.')
+print(f'I felt so {adj1}. My {fam1} {verb3} and {fam2} had to {verb4}. We quietly waited for {time} until it left. We all dashed to the {room} quickly before it might come back again.')
