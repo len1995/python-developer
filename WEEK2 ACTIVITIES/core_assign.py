383573e014c3a657e61fc3bc65d54fd7f617c4e5
Aileen Grace Rabasto <agdrabasto@gmail.com>
2023-09-23 13:58:17 +0800

    rectangle computation added

--- WEEK2 ACTIVITIES/core_assign.py
+++ WEEK2 ACTIVITIES/core_assign.py
@@ -1,5 +1,13 @@
 #author: AILEEN GRACE D. RABASTO
 
-square_length = float(input('What is the length of the side of the square? '))
-area = square_length ** 2
-print(f'The area of the square is : {area:.2f}')
+#computing for the area of the side of the square
+# square_length = float(input('What is the length of the side of the square? '))
+# area = square_length ** 2
+# print(f'The area of the square is : {area:.1f}')
+# print()
+
+# computing  for the area of the rectangle
+rectangle_length = float(input('What is the length of the rectangle? '))
+rectangle_width = float(input('What is the width of the rectangle? '))
+area_rectangle = rectangle_length * rectangle_width
+print(f'The area of the rectangle is: {area_rectangle}')
