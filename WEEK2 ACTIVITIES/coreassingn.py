e8ce70610d4a7809a4c3acb056e14299cfbef29d
Aileen Grace Rabasto <agdrabasto@gmail.com>
2023-09-23 13:50:26 +0800

    assignment

---
+++ WEEK2 ACTIVITIES/coreassingn.py
@@ -0,0 +1,5 @@
+#author: AILEEN GRACE D. RABASTO
+
+square_length = float(input('What is the length of the side of the square? '))
+area = square_length ** 2
+print(f'The area of the square is : {area:.2f}')
