ebbe0aedb4faf7a96c88119af50c14440b977012
Aileen Grace Rabasto <agdrabasto@gmail.com>
2023-09-23 10:45:27 +0800

    additoinal input

--- WEEK2 ACTIVITIES/act1.py
+++ WEEK2 ACTIVITIES/act1.py
@@ -1,3 +1,12 @@
-pipage=int(input('How old are you? '))
-addyr=1
-print(f'On your next birthday, you will be {pipage}+{addyr}')
+# pipage=int(input('How old are you? '))
+# addyr=pipage + 1
+# print(f'On your next birthday, you will be {addyr}.')
+
+# eggc_num=int(input('How many egg cartons do you have? '))
+# eggpc=eggc_num * 12
+# print(f'You have {eggpc} eggs')
+
+n_cookies=int(input('\nHow may cookies do you have? '))
+n_pip=int(input('How many people are there?'))
+each_pip= n_cookies/n_pip
+print(f'Each person may have {each_pip} cookies')
